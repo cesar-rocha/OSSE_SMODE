1967b8cd17d1d78fae336c1dc626895326401b47
Cesar B Rocha <crocha@whoi.edu>
2019-08-28 12:07:47 -0400

    Initializes repository and adds existing codes

---
+++ CalculateSpectra.py
@@ -0,0 +1,32 @@
+from netCDF4 import Dataset
+from scipy.signal import detrend
+import numpy as np
+import matplotlib.pyplot as plt
+import gsw
+
+from SpectralUtils import *
+
+plt.close("all")
+
+# Directory with ROMS output provided by 
+# Roy Barkan to Eric D'Asaro during LASER.
+__datadir__ = "DATA/"
+
+# Surface fields and derived quantities.
+surface = Dataset(__datadir__+"surface_150.nc")
+div     = Dataset(__datadir__+"div150.nc")
+vort    = Dataset(__datadir__+"vort150.nc")
+
+# Model grid (dx ≈ 450 m, dy ≈ 450 m).
+# Vorticity seems to be on the same grid,
+# but divergence has one less point in x.
+lon, lat = surface['lon_rho'][:], surface['lat_rho'][:]
+
+# Calculate wavenumber spectra.
+ntmax = 240         # 10 days
+dx, dy = 0.45, 0.45 # km
+
+spectrau = OneDimSpectra(f=surface['u'][:ntmax,0], dx=0.45,dy=0.45)
+spectrav = OneDimSpectra(f=surface['v'][:ntmax,0], dx=0.45,dy=0.45)
+
+
---
+++ SpectralUtils.py
@@ -0,0 +1,75 @@
+# Utility functions for spectral calculations
+# with UCLA's ROMS output.
+# Cesar B Rocha
+# WHOI, Summer 2019
+
+from scipy.signal import detrend
+import numpy as np
+
+def Wavenumbers(nx,dx):
+    """
+        Calculates the wavenumber given
+        the length of an array nx and the
+        resolution dx.
+
+        Return
+        - wavenumber array
+    """
+    dk = 1./(nx*dx)
+
+    if nx%2:
+        k = dk*np.arange( (nx-1)/2.  + 1 )
+    else:
+        k = dk*np.arange(nx/2+1)
+    return k
+
+def OneDimSpectra(f,dx,dy):
+    """
+        Calculates one dimensional spectra
+        of ROMS model field f.
+
+        Return
+        - specdic: a dictionary with one-dimensional
+                   spectra, label by axes ('x','y'),
+                   with arrays specdic[label]['wavenumber']
+                   and specdic[label]['spectrum'].
+    """
+
+    # Spectra grid
+    nt, ny, nx = f.shape
+   
+    # Loop over axes (y,x) = (1,2)
+    for n,d,axis,label in zip((nx,ny),(dx,dy),(2,1),('x','y')):
+
+        # Get wavenumbers
+        k = Wavenumbers(n,d)
+
+        # Spectral window
+        win  = np.hanning(n)
+        win *= np.sqrt(n/(win**2).sum())
+        if axis == 1:
+            win = win[np.newaxis,:,np.newaxis]
+        elif axis == 2:
+            win = win[np.newaxis,np.newaxis,:]
+
+        # Remove linear trend
+        f = detrend(f,axis=axis, type='linear')
+
+        # Calculate the spectrum
+        fh = np.fft.rfft(f*win, axis=axis)
+        spec = 2*(np.abs(fh)**2)/(n/d)
+
+        # Average over non-Fourier axes  
+        if axis == 1:
+            spec = spec.mean(axis=(0,-1))
+        elif axis == 2:
+            spec = spec.mean(axis=(0,1))
+
+        # Store one-dimensional spectra and waveumber
+        # in a dictionary.
+        try:
+            specdic.update({label: {"wavenumber": k, "spectrum": spec}})
+        except:
+            specdic = {label: {"wavenumber": k, "spectrum": spec}}
+
+    return specdic
